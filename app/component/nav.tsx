d1bcdd6a5540e1900a18f7651f919bc44efb78a4
518651 <1725212799@qq.com>
2023-05-24 15:23:24 +0000

    Task说明

--- app/component/nav.tsx
+++ app/component/nav.tsx
@@ -8,13 +8,20 @@ import { AcmeLogo } from "./AcmeLogo";
 
 export default function Nav(){
     return (
-       <Navbar>
+       <Navbar isBordered >
         <Navbar.Brand>
           <AcmeLogo />
           <Text b color="inherit" hideIn="xs">
             ACME
           </Text>
         </Navbar.Brand>
+        <Navbar.Content hideIn="xs" >
+          <Navbar.Link href="#">Features</Navbar.Link>
+          <Navbar.Link isActive href="#">Customers</Navbar.Link>
+          <Navbar.Link href="#">Pricing</Navbar.Link>
+          <Navbar.Link href="#">Company</Navbar.Link>
+        </Navbar.Content>
+
        </Navbar>
     )
 }
